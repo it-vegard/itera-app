ad18dac88403da3ff7fefab7d4ee5d184c8e56e0
Vegard Haugstvedt <it.vegard@gmail.com>
2016-03-05 06:05:34 +0100

    Refactoring. Using standard navigation drawer as super class.

--- app/src/main/java/no/itera/app/makeadifference/activities/events/course/CreateCourseActivity.java
+++ app/src/main/java/no/itera/app/makeadifference/activities/events/course/CreateCourseActivity.java
@@ -17,9 +17,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import no.itera.app.makeadifference.R;
+import no.itera.app.makeadifference.activities.NavigationDrawerActivity;
 import no.itera.app.makeadifference.activities.events.courselist.CourseListActivity;
 
-public class CreateCourseActivity extends AppCompatActivity
+public class CreateCourseActivity extends NavigationDrawerActivity
     implements NavigationView.OnNavigationItemSelectedListener {
 
   @Override
@@ -49,62 +50,5 @@ public class CreateCourseActivity extends AppCompatActivity
         startActivity(intent);
       }
     });
-  }
-
-  @Override
-  public void onBackPressed() {
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-    } else {
-      super.onBackPressed();
-    }
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.create_course, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  @SuppressWarnings("StatementWithEmptyBody")
-  @Override
-  public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
-    int id = item.getItemId();
-
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
-
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
-
-    }
-
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    drawer.closeDrawer(GravityCompat.START);
-    return true;
   }
 }
